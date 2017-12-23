eed54b443e25e311059f56cd41f2781101988cf8
mackret <kret86@yandex.ru>
2017-12-23 20:41:54 +0300

    evening update

--- src/main/java/MainRep.java
+++ src/main/java/MainRep.java
@@ -2,11 +2,21 @@ public class MainRep {
 
 
 
+static String pathToXLSFile = "//Users//mackret//Downloads/13_выполнено.xlsx";
+    //      "C://Users//SBT-Kretov-AA//Downloads/11_выполнено.xlsx"
+
+static String[]weekEndDays = {"08","10","11"};
 
         public static void main (String[]args) {
 
 
-            FromXLSXToArr.gotFromExcel("//Users//mackret//Downloads/13_выполнено.xlsx");
+//TestDateFormat.testDateFormat(pathToXLSFile);
+
+FromXLSXToArr.gotFromExcel(pathToXLSFile, weekEndDays);
+
+
+//выходные в июне: 3,4,10,11,17,18,24,25
+
 
 
         }
